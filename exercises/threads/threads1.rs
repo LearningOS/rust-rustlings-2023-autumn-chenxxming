6aa0de74d8f0fefc9c9ab1e7e78e6f2385b6f47c
chenxxming <1192744032@qq.com>
2023-10-23 16:04:50 +0800

    update

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -4,30 +4,31 @@
 
 
 use std::thread;
-use std::time::Duration;
-
+use std::time::{Duration, Instant};
 
 fn main() {
-
-    let mut handles  = vec![];
+    let mut handles = vec![];
     for i in 0..10 {
         handles.push(thread::spawn(move || {
-            let start = Instant :: now
+            let start = Instant::now();
             thread::sleep(Duration::from_millis(250));
             println!("thread {} is complete", i);
             start.elapsed().as_millis()
         }));
     }
 
-    let mut completed_threads = 0;
+    let mut results: Vec<u128> = vec![];
     for handle in handles {
         // TODO: a struct is returned from thread::spawn, can you use it?
-        let result = handle.join().unwrap();
-        results.push(result);
+        results.push(handle.join().unwrap())
     }
 
-    if completed_threads != 10 {
+    if results.len() != 10 {
         panic!("Oh no! All the spawned threads did not finish!");
     }
-    
+
+    println!();
+    for (i, result) in results.into_iter().enumerate() {
+        println!("thread {} took {}ms", i, result);
+    }
 }
